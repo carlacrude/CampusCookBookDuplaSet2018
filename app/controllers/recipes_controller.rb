b54cf5b7660039ce9becf9fc22e841fd8c1fcf9a
Carla Crude <carlacrude@gmail.com>
2018-09-11 20:08:45 -0300

    Usuário edita uma receita

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -16,4 +16,16 @@ class RecipesController < ApplicationController
         redirect_to recipe_path(@recipe.id)
     end
 
+    def edit
+        @recipe = Recipe.find(params[:id])
+    end
+
+    def update
+        @recipe = Recipe.find(params[:id])
+        @recipe.update(params.require(:recipe).permit(:title, :recipe_type, :cuisine, 
+                                                    :difficulty, :cook_time, :ingredients, 
+                                                    :cook_method))
+                                                    
+        redirect_to recipe_path(@recipe.id)
+    end
 end
