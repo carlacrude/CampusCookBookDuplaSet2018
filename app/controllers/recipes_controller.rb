f2a4882408349e8a9f4d69199e58a39f9a5d2a95
Carla Crude <carlacrude@gmail.com>
2018-09-12 22:23:57 -0300

    [WIP] - Usuário usa tipos de receitas cadastradas

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,37 +1,44 @@
+
 class RecipesController < ApplicationController
+    
     def show
         @recipe = Recipe.find(params[:id])
     end
 
     def new
+        @recipetypes = RecipeType.all
         @recipe = Recipe.new
     end
     
     def create
-        @recipe = Recipe.new(params.require(:recipe).permit(:title, :recipe_type, :cuisine, 
-                                                            :difficulty, :cook_time, :ingredients, 
-                                                            :cook_method))
         
+        @recipe = Recipe.new(recipe_params)
         if @recipe.save
             redirect_to recipe_path(@recipe.id)
         else
+            @recipetypes = RecipeType.all
             render 'new'
         end
     end
 
     def edit
         @recipe = Recipe.find(params[:id])
+        @recipetypes = RecipeType.all
     end
 
     def update
-        @recipe = Recipe.find(params[:id])
-        
-        if @recipe.update(params.require(:recipe).permit(:title, :recipe_type, :cuisine, 
-                                                    :difficulty, :cook_time, :ingredients, 
-                                                    :cook_method))                                            
+        @recipe = Recipe.find(params[:id])  
+        if @recipe.update(recipe_params)                                            
         redirect_to recipe_path(@recipe.id)
         else
+            @recipetypes = RecipeType.all
             render 'edit'
         end
+    end
+
+    def recipe_params
+        params.require(:recipe).permit(:title, :recipe_type_id, :cuisine, 
+                                                    :difficulty, :cook_time, :ingredients, 
+                                                    :cook_method)
     end
 end
